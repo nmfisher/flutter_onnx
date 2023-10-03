825836d3165379a7d552a3bf9e71c4c12e0fc206
Nick Fisher <nick.fisher@polyvox.app>
2023-10-03 22:15:07 +0800

    update example project

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -25,33 +25,14 @@ class _MyAppState extends State<MyApp> {
   @override
   void initState() {
     super.initState();
-    if (!Platform.isLinux) {
+    if (Platform.isLinux) {
       _dl = DynamicLibrary.open(File(Platform.resolvedExecutable).parent.path +
           "/lib/libonnxruntime.so.1.11.0");
+    } else if (Platform.isAndroid) {
+      _dl = DynamicLibrary.open("libonnxruntime.so");
+    } else {
+      _dl = DynamicLibrary.process();
     }
-    initPlatformState();
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    // We also handle the message potentially returning null.
-    try {
-      platformVersion =
-          await FlutterOnnx.platformVersion ?? 'Unknown platform version';
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _platformVersion = platformVersion;
-    });
   }
 
   @override
